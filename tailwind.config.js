1c87fef8efae73be655d047e5acb17a94ab566f9
alexus_g1 <antidot2002@yandex.ru>
2023-06-26 11:47:44 +0300

    add styles to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,33 @@ module.exports = {
     "./dist/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
+    container: {
+      screens: {
+        'lg' :'1400px'
+      },
+      center: true,
+    },
     fontFamily: {
       circe: ["circe"]
     },
     extend: {
+      backgroundImage: {
+        'texture': "url('/img/texture.png')",
+        'BG_3': "url('/img/BG_3.png')",
+        'bg-4': "url('/img/bg4.png')",
+        'laptop': "url('/img/laptop-bg.png')",
+        'tablet': "url('/img/tablet-bg.png')",
+        'mobile': "url('/img/mobile-bg.png')",
+      },
       fontSize: {
           '10xl': ['180px', '180px'],
       },
       spacing: {
-        'chair': '1024px'
+        'texture': '1262px',
+        'chair': '1024px',
+        'laptop': '1506px',
+        'tablet':'1633px',
+        'mobile': '1416px',
       }
     },
   },
